3cd488a5ba23a48a2db7d9494c841360afcfefe7
Luiz Picolo <luizpicolo@gmail.com>
2018-10-19 12:35:33 -0400

    Fixed to test original code

--- spec/bun_bo_spec.rb
+++ spec/bun_bo_spec.rb
@@ -10,11 +10,11 @@ RSpec.describe BunBo do
     it "should create a spec file" do
       BunBo.new.run(@input)
       expect(File.exist?(@output)).to be(true)
-      FileUtils.rm_rf(@output)
+      File.delete(@output)
     end
 
     it "should not create if path file does not exist" do
-      expect(BunBo.new.run('path/error.rb')).to eq('File does not exists')
+      expect{ BunBo.new.run('path/error.rb')}.to output(/File does not exists/).to_stdout
     end
   end
 end
