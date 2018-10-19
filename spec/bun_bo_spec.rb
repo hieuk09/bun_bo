b57b38495451940f18726c2d58cca9a6378eb915
Luiz Picolo <luizpicolo@gmail.com>
2018-10-19 11:11:54 -0400

    Fixed: Update details for new version gem

--- spec/bun_bo_spec.rb
+++ spec/bun_bo_spec.rb
@@ -3,14 +3,14 @@ require "bun_bo"
 RSpec.describe BunBo do
   before do
     @input = "spec/fixtures/example.rb"
-    @output = "spec/spec/fixtures/example_spec.rb"
+    @output = "spec/fixtures/example_spec.rb"
   end
 
   describe "#run" do
     it "should create a spec file" do
       BunBo.new.run(@input)
       expect(File.exist?(@output)).to be(true)
-      FileUtils.rm_rf('spec/spec')
+      FileUtils.rm_rf(@output)
     end
 
     it "should not create if path file does not exist" do
